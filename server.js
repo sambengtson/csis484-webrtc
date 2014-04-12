93fafaae47d8fb3e99154c012a2d04408a3445a9
sambengtson <sam.bengtson@gmail.com>
2014-04-12 16:07:04 -0500

    removed unneccessary logging

--- server.js
+++ server.js
@@ -11,38 +11,23 @@ io.set('log level', 1);
 io.sockets.on('connection', function(socket) {
 
     console.log('client connected');
-    function log() {
-        var array = [">>> Message from server: "];
-        for (var i = 0; i < arguments.length; i++) {
-            array.push(arguments[i]);
-        }
-        socket.emit('log', array);
-    }
-
     socket.on('message', function(message) {
-        log('Got message: ', message);
-        // For a real app, should be room only (not broadcast)
         socket.broadcast.emit('message', message);
     });
 
     socket.on('create or join', function(room) {
         var numClients = io.sockets.clients(room).length;
 
-        log('Room ' + room + ' has ' + numClients + ' client(s)');
-        log('Request to create or join room', room);
+        console.log('Room ' + room + ' has ' + numClients + ' client(s)');
 
-        if (numClients == 0) {
+        if (numClients === 0) {
             socket.join(room);
             socket.emit('created', room);        
-        } else { // max two clients
+        } else { 
             io.sockets.in(room).emit('join', room);
             socket.join(room);
             socket.emit('joined', room);
         }
-        socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-        socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
-
     });
-
 });
 
