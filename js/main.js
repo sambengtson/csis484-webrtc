e5ab80563a2584048696590b95a39e595f801895
sambengtson <sam.bengtson@gmail.com>
2014-04-29 19:17:16 -0500

    Fixed using public URL for io connection

--- js/main.js
+++ js/main.js
@@ -29,8 +29,8 @@ function beginVideo(roomName, callback) {
 
 /////////////////////////////////////////////
 
-    //var socket = io.connect('http://dev.t1innovations.com:2013');
-    var socket = io.connect('http://192.168.1.5:2013');
+    var socket = io.connect('http://sambengtson.com:2013');
+    //var socket = io.connect('http://192.168.1.5:2013');
 
     if (roomName !== '') {
         console.log('Create or join room', roomName);
